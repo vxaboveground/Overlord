09576231c913db79f6b39d5b16900ed0558eb4af
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2026-05-13 23:25:49 -0500

    allow domains to get ips of users instead of docker 172

--- Overlord-Server/src/server/http-dispatch.ts
+++ Overlord-Server/src/server/http-dispatch.ts
@@ -1,3 +1,5 @@
+import { wrapServerWithClientIp, type RequestServerLike } from "./client-ip";
+
 export type RouteHandler = (req: Request, url: URL, server: unknown) => Promise<Response | null>;
 
 export function createHttpFetchHandler(deps: {
@@ -11,8 +13,9 @@ export function createHttpFetchHandler(deps: {
         return new Response("", { headers: deps.CORS_HEADERS });
       }
       const url = new URL(req.url);
+      const wrapped = wrapServerWithClientIp(server as RequestServerLike);
       for (const route of deps.routes) {
-        const response = await route(req, url, server);
+        const response = await route(req, url, wrapped);
         if (response) return response;
       }
       return new Response("Not found", { status: 404 });
