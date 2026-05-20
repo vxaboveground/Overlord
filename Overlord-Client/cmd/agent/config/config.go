3aa6194672dc351560b9873af25b6a5591bfd049
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2026-05-19 20:32:01 -0500

    2.2.11

--- Overlord-Client/cmd/agent/config/config.go
+++ Overlord-Client/cmd/agent/config/config.go
@@ -17,7 +17,7 @@ import (
 	"time"
 )
 
-var AgentVersion = "2.2.10"
+var AgentVersion = "2.2.11"
 
 var DefaultPersistence = "false"
 var DefaultServerURL = "wss://127.0.0.1:5173"
--- Overlord-Server/src/version.ts
+++ Overlord-Server/src/version.ts
@@ -1,1 +1,1 @@
-export const SERVER_VERSION = "2.2.10";
+export const SERVER_VERSION = "2.2.11";
